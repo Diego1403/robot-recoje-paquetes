68595f99f387c7229c942e87e2b13aa93b2af044
Diego Vasquez <dvr00010@red.ujaen.es>
2024-04-22 18:26:20 +0200

    server update and connected to mqtt

--- robot_buscador/server.py
+++ robot_buscador/server.py
@@ -1,39 +1,22 @@
 import paho.mqtt.client as mqtt
+import time
 
 # MQTT broker credentials
-broker_address = "192.168.78.10"
+broker_address = "192.168.48.245"
 broker_port = 1883
-client_id = "your_client_id" # Change this to a unique identifier for your client
-username = "" # If authentication is required, provide username
-password = "" # If authentication is required, provide password
+topic = "map"
 
-# Callback function to handle MQTT connection
 def on_connect(client, userdata, flags, rc):
-    print("Connected to MQTT broker with result code "+str(rc))
-    # Subscribe to the map topic
-    client.subscribe("map")
-    
+    print("Connected with result code "+str(rc))
+    client.subscribe(topic)
 
-# Callback function to handle MQTT message reception
 def on_message(client, userdata, msg):
-    if msg.topic == "map":
-        map_data = msg.payload.decode("utf-8") # Decode the received map data
-# Process the received map data (decode the map structure, update robot's internal map, etc.)
-        print("Received map data:", map_data)
+    print(msg.topic+" "+str(msg.payload))
 
-# Create MQTT client instance
-client = mqtt.Client(client_id=client_id)
-
-# Set MQTT username and password if authentication is required
-if username and password:
-    client.username_pw_set(username, password)
-
-# Set callback functions
+client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-# Connect to MQTT broker
-client.connect(broker_address, broker_port)
+client.connect(broker_address, broker_port, 60)
 
-# Start MQTT network loop
 client.loop_forever()
